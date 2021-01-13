9c47bbac5593cf3d42c97d0bf36be1751417bb1d
Jolie Hobloss <jolieshobloss@gmail.com>
2021-01-13 12:22:07 +0000

    Automatically backed up by Learn

--- src/components/MovieShow.js
+++ src/components/MovieShow.js
@@ -1,10 +1,11 @@
 import React from 'react';
- 
-const MovieShow = props => {
- 
+
+const MovieShow = ({match, movies}) => {
   return (
     <div>
-      <h3>Movies Show Component!</h3>
+      // And here we access the `movieId` stored in match.params to render 
+      // information about the selected movie
+      <h3>{ movies[match.params.movieId].title }</h3>
     </div>
   );
 }
