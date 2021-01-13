5c9370611c8cb17d81f618f86f02992b487953b0
Jolie Hobloss <jolieshobloss@gmail.com>
2021-01-13 12:21:52 +0000

    Automatically backed up by Learn

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -7,8 +7,7 @@ import MovieShow from '../components/MovieShow';
 const MoviesPage = ({ match, movies }) => (
   <div>
     <MoviesList movies={movies} />
-    // Here we replace the `component` prop with the `render` prop so we can pass the 
-    // route information to the `MovieShow` component
+  
     <Route path={`${match.url}/:movieId`} render={routerProps => <MovieShow {...routerProps} movies={movies} /> }/>
   </div>
 )
